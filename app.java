6ba165be65030e498755def2caf56c2a5141a903
Halila Nakaziba <shakuze2009@yahoo.com>
2022-11-22 12:37:39 +0000

    first commit

---
+++ app.java
@@ -0,0 +1,4 @@
+#Ya Allah please make this course easy and understable for us
+#Let it not be time consuming for us so we cnan have the time to concetrate on deen
+#Make it benefitial for us in the duniya and in the hereafter
+#Ameena ya rabee
